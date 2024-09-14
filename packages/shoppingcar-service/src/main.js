279c4b1d4163938165c216765e0dfb47918afa61
PassonoTW <82664528+passontw@users.noreply.github.com>
2024-09-14 19:35:41 +0800

    chore(增加 Swaggerui ):
    http://localhost:3333/api-docs

--- packages/shoppingcar-service/src/main.js
+++ packages/shoppingcar-service/src/main.js
@@ -9,10 +9,41 @@ import morgan from 'morgan';
 import passport from 'passport';
 import cookieParser from 'cookie-parser';
 import * as path from 'path';
-import authRouter from './controllers/authRouter';
+import appRouter from './controllers/app';
+import consoleRouter from './controllers/console';
 import userRouter from './controllers/userRouter';
+import seaggerUI from 'express-swagger-generator';
+import packageJson from '../../../package.json';
 
-const {NODE_ENV = "development"} = process.env;
+const {NODE_ENV = "development", APP_DOMAIN} = process.env;
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: 'Shoppingcar API Service',
+      description: '購物車 api',
+      version: `${packageJson.version} - ${NODE_ENV}`,
+    },
+    host: APP_DOMAIN,
+    produces: ['application/json'],
+    schemes: ['http', 'https'],
+    securityDefinitions: {
+      JWT: {
+        type: 'apiKey',
+        in: 'header',
+        name: 'Authorization',
+        description: 'JWT token',
+      },
+    },
+  },
+  route: {
+    url: '/api-docs',
+    docs: '/api-docs.json',
+  },
+  basedir: __dirname,
+  files: ['../../../packages/shoppingcar-service/src/controllers/**/*.js'],
+};
+
 const app = express();
 
 if (NODE_ENV === "development") {
@@ -32,8 +63,11 @@ app.get('/api', (req, res) => {
   res.json({ message: 'Welcome to shoppingcar-service!!!' });
 });
 
-app.use('/auth', authRouter);
+app.use('/app', appRouter);
+app.use('/console', consoleRouter);
 app.use('/users', passport.authenticate('jwt', { session: false }), userRouter);
+
+seaggerUI(app)(swaggerOptions);
 
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
