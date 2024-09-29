5bd0b958b585fa42c2b64b519a583fde7fc1a8fb
PassonoTW <82664528+passontw@users.noreply.github.com>
2024-09-29 15:27:31 +0800

    feat: #17 新增會員功能完成

--- packages/shoppingcar-service/src/controllers/console/userRouter.js
+++ packages/shoppingcar-service/src/controllers/console/userRouter.js
@@ -1,7 +1,54 @@
+import pick from "lodash/pick";
 import {responseErrWithMsg, responseOk} from "~/helpers/response";
 import {createUserRequestSchema} from "~/helpers/schemas";
 import {createUserService} from "~/services/userServices";
 
+
+/**
+ * @typedef ConsoleCreateUserRequest
+ * @property {string} name.required
+ *   - merchant name
+ *   - eg: testmerchant001
+ * @property {string} phone.required
+ *   - merchant phone
+ *   - eg: 0987654321
+ * @property {string} password.required
+ *   - password: 6 ~ 20 個英數組合
+ *   - eg: a12345678
+ */
+
+/**
+ * @typedef ConsoleUserInformation
+ * @property {number} id.required
+ *  - console merchant Id
+ *  - eg: 1
+ * @property {string} name.required
+ *   - merchant name
+ *   - eg: testmerchant001
+ * @property {string} phone.required
+ *   - merchant phone
+ *   - eg: 0987654321
+ */
+
+/**
+ * @typedef ConsoleCreateUserResponse
+ * @property {ConsoleUserInformation.model} item.required 
+ *  - console merchant information
+ */
+
+/**
+ * Create User API.
+ * @group ConsoleUser
+ * @route POST /console/users
+ * @param {ConsoleCreateUserRequest.model} data.body.required - the new point
+ * @returns {ConsoleCreateUserResponse.model} 200 - success, return requested data
+ * @returns {String} 400 - invalid request params/query/body
+ * @returns {String} 404 - required data not found
+ * @returns {Error} 500 - unexpected error
+ * @security JWT
+ * @typedef ConsoleCreateUserResponse
+ * @property {{integer}} code - response code - eg: 200
+ */
 const createUserRouter = async (req, res) => {
   try {
     const createUserRequest = await createUserRequestSchema.validate(req.body);
@@ -11,7 +58,7 @@ const createUserRouter = async (req, res) => {
       item
     });
   }catch(error) {
-    return responseErrWithMsg(res, err);
+    return responseErrWithMsg(res, error.message);
   }
 };
 
