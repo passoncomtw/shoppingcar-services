da06ec5be53dbb1d0c9274b36efcc48a734fc8b8
PassonoTW <82664528+passontw@users.noreply.github.com>
2024-10-04 14:58:56 +0800

    feat: #47 會員詳情 UI 完成

--- packages/shoppingcar-app/src/app/constants/naviagtions.js
+++ packages/shoppingcar-app/src/app/constants/naviagtions.js
@@ -1,5 +1,6 @@
-import * as React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { IconButton, MD3Colors } from 'react-native-paper';
+import FontAwesomeIcon from "react-native-vector-icons/FontAwesome5";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
@@ -14,7 +15,10 @@ const Tab = createMaterialBottomTabNavigator();
 
 const HomeStackNavigator = () => (
   <Stack.Navigator>
-    <Stack.Screen name="Home" component={HomeScreen} />
+    <Stack.Screen
+      name="Home"
+      component={HomeScreen}      
+    />
   </Stack.Navigator>
 );
 
@@ -32,53 +36,74 @@ const ShoppingcarStackNavigator = () => (
 
 const SettingStackNavigator = () => (
   <Stack.Navigator>
-    <Stack.Screen name="Setting" component={SettingScreen} />
+    <Stack.Screen
+      name="Setting"
+      component={SettingScreen}
+      options={{
+        title: "",
+        headerLeft: () => (
+          <IconButton
+            icon={() => <FontAwesomeIcon name="bars" size={20} />}
+            iconColor={MD3Colors.grey}
+            size={20}
+            onPress={() => console.log('Pressed')}
+          />
+        ),
+      }}
+    />
   </Stack.Navigator>
 );
 
 export const AuthNavigation = () => (
   <Tab.Navigator>
     <Tab.Screen
-        name="HomeTab"
-        component={HomeStackNavigator}
-        options={{
-          tabBarLabel: '',
-          tabBarIcon: ({ focus, color }) => (
-            <MaterialCommunityIcons name="home" color={color} size={26} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="OrderTab"
-        component={OrderStackNavigator}
-        options={{
-          tabBarLabel: '',
-          tabBarIcon: ({ focus, color }) => (
-            <MaterialCommunityIcons name="border-all" color={color} size={26} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="ShoppingcarTab"
-        component={ShoppingcarStackNavigator}
-        options={{
-          tabBarLabel: '',
-          tabBarIcon: ({ focus, color }) => (
-            <MaterialCommunityIcons name="cart" color={color} size={26} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="SettingTab"
-        component={SettingStackNavigator}
-        options={{
-          tabBarLabel: '',
-          tabBarIcon: ({ focus, color }) => (
-            <MaterialCommunityIcons name="account" color={color} size={26} />
-          ),
-        }}
-      />
-  </Tab.Navigator>  
+      name="HomeTab"
+      component={HomeStackNavigator}
+      options={{
+        tabBarLabel: '',
+        tabBarIcon: ({ focus, color }) => (
+          <MaterialCommunityIcons name="home" color={color} size={26} />
+        ),
+        headerRight: () => (
+          <Button
+            onPress={() => alert('This is a button!')}
+            title="Info"
+            color="#fff"
+          />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="OrderTab"
+      component={OrderStackNavigator}
+      options={{
+        tabBarLabel: '',
+        tabBarIcon: ({ focus, color }) => (
+          <MaterialCommunityIcons name="border-all" color={color} size={26} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="ShoppingcarTab"
+      component={ShoppingcarStackNavigator}
+      options={{
+        tabBarLabel: '',
+        tabBarIcon: ({ focus, color }) => (
+          <MaterialCommunityIcons name="cart" color={color} size={26} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="SettingTab"
+      component={SettingStackNavigator}
+      options={{
+        tabBarLabel: '',
+        tabBarIcon: ({ focus, color }) => (
+          <MaterialCommunityIcons name="account" color={color} size={26} />
+        ),
+      }}
+    />
+  </Tab.Navigator>
 );
 
 export const UnauthNavigation = () => (
--- packages/shoppingcar-app/src/app/screens/SettingScreen/view.js
+++ packages/shoppingcar-app/src/app/screens/SettingScreen/view.js
@@ -1,13 +1,31 @@
-import React from "react";
-import Logo from "../../components/Logo";
+import { Avatar, List } from "react-native-paper";
 import Background from "../../components/Background";
-import Header from "../../components/Header";
+import Button from '../../components/Button';
 
-export default function SettingScreen() {  
+const SettingScreen = () => {
   return (
-    <Background>
-      <Logo />
-      <Header>Shoppingcar Screen</Header>      
+    <Background containerStyle={{alignSelf: 'center'}}>
+      <Avatar.Image size={88} source={require('../../../assets/girlLogo.jpg')} />
+      <List.Item
+        title="姓名"
+        description="Testuser001"
+        left={() => <List.Icon icon="account" />}
+      />
+      <List.Item
+        title="電話"
+        description="0987654321"
+        left={() => <List.Icon icon="phone" />}
+      />
+      <List.Item
+        title="信箱"
+        description="aaa@bbb.ccc"
+        left={() => <List.Icon icon="email" />}
+      />
+      <Button mode="contained" onPress={() => alert("登出")}>
+        登出
+      </Button>
     </Background>
   );
-}
+};
+
+export default SettingScreen;
