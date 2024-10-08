7b255c98a37828e60b85baeb85ac187c77b1fbe5
TomasLin <horsekit1982@gmail.com>
2024-10-08 13:11:44 +0800

    feat: #9 取回店家的單一商品
    測試案例
    * 取回資料後格式檢查

--- packages/shoppingcar-service/src/__tests__/controllers/app/merchantRouter.test.js
+++ packages/shoppingcar-service/src/__tests__/controllers/app/merchantRouter.test.js
@@ -9,6 +9,9 @@ import { token } from "~/constants/mockDatas/settings";
 
 const request = supertest.agent(server);
 
+const MERCHANT_ID = 57;
+const PRODUCT_ID = 57;
+
 describe("測試 App 店家 功能", () => {
   afterAll((done) => {
     server.close();
@@ -38,7 +41,7 @@ describe("測試 App 店家 功能", () => {
 
   it("should get app merchant products success", async () => {
     return request
-    .get("/app/merchants/57/products")
+    .get(`/app/merchants/${MERCHANT_ID}/products`)
     .set({ Authorization: token })
     .query({pageSize: 1})
     .expect(200)
@@ -49,6 +52,31 @@ describe("測試 App 店家 功能", () => {
       expect(items.length<=1).toBe(true);
       expect(isNumber(totalCount)).toBe(true);
       expect(isEmpty(pageInfo)).toBe(false);
+    });
+  });
+  
+  it("should get app merchant product information success", async () => {
+    return request
+    .get(`/app/merchants/${MERCHANT_ID}/products/${PRODUCT_ID}`)
+    .set({ Authorization: token })
+    .query({})
+    .then((res) => {
+      expect(res.statusCode).toBe(200);
+      const { item } = res.body;
+
+      expect(item.id).toBe(PRODUCT_ID);
+      expect(isNumber(item.price)).toBe(true);
+      expect(isNumber(item.stockAmount)).toBe(true);
+      expect(isString(item.name)).toBe(true);
+      expect(isString(item.subtitle)).toBe(true);
+      expect(isString(item.description)).toBe(true);
+
+      const {merchant} = item;
+      expect(isEmpty(merchant)).toBe(false);
+      expect(merchant.id).toBe(MERCHANT_ID);
+      expect(isString(merchant.name)).toBe(true);
+      expect(isString(merchant.email)).toBe(true);
+      expect(isString(merchant.phone)).toBe(true);
     });
   });
 });
