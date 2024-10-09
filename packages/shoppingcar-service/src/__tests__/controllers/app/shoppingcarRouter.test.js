f644fd8b3d3e4e5dcfaf9bedde7beac2e04caaac
TomasLin <horsekit1982@gmail.com>
2024-10-09 18:42:25 +0800

    feat: #10 取回購物車商品列表測試安利完成
    * 成功取回購物車的產品資訊

--- packages/shoppingcar-service/src/__tests__/controllers/app/shoppingcarRouter.test.js
+++ packages/shoppingcar-service/src/__tests__/controllers/app/shoppingcarRouter.test.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+import isNumber from "lodash/isNumber";
+import isString from "lodash/isString";
 import supertest from "supertest";
 import server from "~/main";
 import { token } from "~/constants/mockDatas/settings";
@@ -23,6 +25,40 @@ describe("測試 會員 購物車 功能", () => {
       .send({ amount: 2 })
       .then((res) => {
         expect(res.statusCode).toBe(200);
+      })
+  });
+
+  it("should get products from shoppingcar success", async () => {
+    return request
+      .get("/app/shoppingcars")
+      .set({ Authorization: token })
+      .then((res) => {
+        expect(res.statusCode).toBe(200);
+        const {item} = res.body;
+        expect(isNumber(item.id)).toBe(true);
+        expect(isNumber(item.totalAmount)).toBe(true);
+        expect(isNumber(item.productCount)).toBe(true);
+        const {user, shoppingcarItems} = item;
+        expect(isNumber(user.id)).toBe(true);
+        expect(isString(user.name)).toBe(true);
+        expect(isString(user.phone)).toBe(true);
+
+        shoppingcarItems.map(shoppingcarItem => {
+            expect(isNumber(shoppingcarItem.id)).toBe(true);
+            expect(isNumber(shoppingcarItem.amount)).toBe(true);
+            const {product, merchant} = shoppingcarItem;
+            expect(isNumber(product.id)).toBe(true);
+            expect(isNumber(product.price)).toBe(true);
+            expect(isNumber(product.stockAmount)).toBe(true);
+            expect(isString(product.name)).toBe(true);
+            expect(isString(product.description)).toBe(true);
+            expect(isString(product.subtitle)).toBe(true);
+
+            expect(isNumber(merchant.id)).toBe(true);
+            expect(isString(merchant.name)).toBe(true);
+            expect(isString(merchant.phone)).toBe(true);
+            expect(isString(merchant.email)).toBe(true);
+        });
       })
   });
 });
